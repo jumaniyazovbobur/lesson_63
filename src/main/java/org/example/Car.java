3a9c25147faca46c646199f66d2d7c50a86b6a80
RakhmonaliAnaqulov <rakhmonaliandaqulov@gmail.com>
2023-03-04 21:57:40 +0500

    test7

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,17 +1,12 @@
 package org.example;
 
-import java.util.StringTokenizer;
-
 public class Car {
     private Integer id;
     private String name;
     private String surname;
     private String color;
     private String level;
-    private String age1;
-    private Integer age;
     private String brand;
-    private String magimagi+++++++++++;
-    
+    private Integer car_number;
 
 }
