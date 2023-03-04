39649256a1fcbdc35dd3e754ffd06cced735ead4
ErkinovMoxirbek <erkinov.0021@gmail.com>
2023-03-04 22:13:17 +0500

    getter setter

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 public class Car {
@@ -11,5 +12,69 @@ public class Car {
     private String brand;
     private String model;
 
-    private Date Brithday;
+    private LocalDateTime Brithday;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public LocalDateTime getBrithday() {
+        return Brithday;
+    }
+
+    public void setBrithday(LocalDateTime brithday) {
+        Brithday = brithday;
+    }
 }
