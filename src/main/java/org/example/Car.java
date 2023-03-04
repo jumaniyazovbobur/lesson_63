908b69386042182f4bd14120a34ed8ae07e71b96
SoliyevMuhammad <soliyevmuhammad137@gmail.com>
2023-03-04 08:58:23 -0800

    im here sdh

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -5,7 +5,13 @@ public class Car {
     private String name;
     private String surname;
     private String color;
+
+
     private String level;
     private Integer age;
     private String brand;
+
+    public static void main(String[] args) {
+        System.out.println("Hello wordl ");
+    }
 }
