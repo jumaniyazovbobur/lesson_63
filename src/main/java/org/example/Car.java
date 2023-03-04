144912e4c8fdb326d01022e2b9f03200b567e56c
Developer0020 <bobir.6119@mail.ru>
2023-03-04 22:16:11 +0500

    test_5

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -4,10 +4,7 @@ public class Car {
     private Integer id;
     private String name;
     private String surname;
-    private String color;
-    private String level;
-    private String brand;
-    private String model;
-
-    private Integer name_id;
+    public void main(){
+        System.out.println("mss");
+    }
 }
