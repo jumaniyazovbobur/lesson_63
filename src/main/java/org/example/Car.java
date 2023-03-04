d603c42aedabad4278917eb8182a3eb7c53a8535
jumaniyazovbobur <jumaniyazov.bobur@mail.ru>
2023-03-04 21:52:09 +0300

    test 4

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -7,7 +7,9 @@ public class Car {
     private String name;
     private String surname;
     private String color;
+    private String email;
     private String level;
+    private String group;
     private String age1;
     private Integer age;
     private String brand;
